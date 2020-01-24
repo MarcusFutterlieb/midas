3886a92a03473d29f05e245385db5bba7ab6d9f4
Marcus Futterlieb <16731186+MarcusFutterlieb@users.noreply.github.com>
2020-01-24 23:54:47 +0100

    added rowCount to littleHelper

--- littleHelper.py
+++ littleHelper.py
@@ -6,6 +6,7 @@ Created on Fri Jan 24 2020
 """
 
 import os;
+import csv;
 #from os.path import isfile, join
 
 
@@ -15,3 +16,18 @@ def libraryParser(path):
 #    for item in onlyfiles:
 #        print(item);
     print(onlyfiles);
+
+
+
+def rowCount(file):
+    with open(file, 'r') as t2:
+        reader = csv.reader(t2, delimiter=",");
+        data = list(reader);
+        rowCount = len(data);
+        # print(rowCount);
+        # print('rowcount OK');
+        if (rowCount != 32):
+            print("Error: issue with rowcount");
+            return (999);
+        else:
+            return (rowCount);
