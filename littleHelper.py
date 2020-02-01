353019e42b334349362068f87b8ae00e1f9600a6
Marcus Futterlieb <16731186+MarcusFutterlieb@users.noreply.github.com>
2020-02-01 17:53:14 +0100

    added functions and fixed a overview merging problem

--- littleHelper.py
+++ littleHelper.py
@@ -5,10 +5,11 @@ Created on Fri Jan 24 2020
 @author: Marcus Futterlieb
 """
 
-import os;
-import csv;
+import os
+import csv
 #from os.path import isfile, join
 from pandas import DataFrame
+import pandas
 
 
 
@@ -16,7 +17,7 @@ def libraryParser(path):
     onlyfiles = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
 #    for item in onlyfiles:
 #        print(item);
-    print(onlyfiles);
+    print(onlyfiles)
 
 
 
@@ -60,4 +61,45 @@ def writeToCSV (stockName, stockDates, stockOpen,stockHigh ,stockLow, stockClose
     export_csv1 = df1.to_csv(r'./library/' + stockName + '__hist__new.csv', index=None, header=True)
     export_csv2 = df2.to_csv(r'./library/' + stockName + '__divi__new.csv', index=None, header=True)
     export_csv3 = df3.to_csv(r'./library/' + stockName + '__perf__new.csv', index=None, header=True)
-    print ('writeToCSV: ', stockName, ' has been written into your personal library')
+    print ('writeToCSV: ', stockName, '__new has been written into your personal library')
+
+
+def overviewUpdate (stockList):
+    if not (os.path.exists('library/overview.csv')):
+        print('overviewUpdate: overview does not exist --> creating ...')
+        bundledInformation = {'stockName': stockList, 'prefValue': zerolistmaker(len(stockList))}
+        df = DataFrame(bundledInformation, columns=['stockName', 'prefValue'])
+        export_csv = df.to_csv(r'./library/overview.csv', index=None, header=True)
+    else:
+        updateOverview = False
+        print('overviewUpdate: overview does exist --> updating ...')
+        historicValues_old = pandas.read_csv('./library/overview.csv')
+        stockList_old = historicValues_old['stockName']
+        for cnt_new in range(0, len(stockList)):
+            cnt_tmp = 0
+            print('overviewUpdate: searching ', stockList[cnt_new])
+            for cnt_old in range(0, len(stockList_old)):
+                if (stockList_old.iloc[cnt_old]==stockList[cnt_new]):
+                    print('overviewUpdate: found ', stockList[cnt_new])
+                    break
+                else:
+                    cnt_tmp = cnt_tmp+1
+                    #print('overviewUpdate: counter ', cnt_tmp, '  counterlimit ', len(stockList_old))
+                    if (cnt_tmp>=len(stockList_old)):
+                        print('overviewUpdate: NOT found ', stockList[cnt_new], ' --> appending...')
+                        historicValues_old = historicValues_old.append([stockList[cnt_new], 0])
+                        print('overviewUpdate: new dataframe: ', historicValues_old)
+                        updateOverview = True
+
+
+        if (updateOverview==True):
+            print('overviewUpdate: new dataframe: ', historicValues_old)
+            export_csv1 = historicValues_old.to_csv(r'./library/overview.csv', index=None, header=True)
+            print('overviewUpdate: had to rewrite the overview.csv')
+            # TODO --> test if this works
+
+
+
+def zerolistmaker(n):
+    listofzeros = [0] * n
+    return listofzeros
